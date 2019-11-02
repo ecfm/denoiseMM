fix entropy calculation

main.py
@@ -196,10 +196,14 @@ def train_model(config_file_name, model_name):
             # copying models
             c_output = adv_copy(words)
             f_output = net_copy(words, covarep, facet, inputLen)
+            entropy_c_output = np.mean(sum(c_output * np.log(c_output)))
+            entropy_f_output = np.mean(sum(f_output * np.log(f_output)))
 
             net_adv_loss = cross_entropy(outputs, w_output)
-
             net_adv_loss.backward()
+
+            c_f_loss = (entropy_f_output - entropy_c_output)
+            c_f_loss.backward()
 
             output_all.extend(outputs.tolist())
             label_all.extend(labels.tolist())