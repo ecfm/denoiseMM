56eea0b49e25900123eb2706b3b1c216557fb343
mao <maocf1993@gmail.com>
2019-12-02 12:45:40 -0500

    mask train

--- main.py
+++ main.py
@@ -24,6 +24,7 @@ output_dim_dict = {
     'mosei_senti': 1,
     'iemocap': 8
 }
+np.random.seed(0)
 
 def stopTraining(signum, frame):
     global savedStdout
@@ -194,6 +195,11 @@ def train_model(args, config_file_name, model_name):
             words, covarep, facet, inputLen, labels = data
             if covarep.size()[0] == 1:
                 continue
+            mask_ratio = np.random.choice([0, 0.2, 0.4], p=[0.7, 0.2, 0.1])
+            if mask_ratio > 0:
+                m = np.random.choice([0, 1], p=[mask_ratio, 1 - mask_ratio],
+                                     size=(words.shape[0], words.shape[1], 1))
+                words = words * torch.tensor(m.astype(np.float32))
             words, covarep, facet, inputLen, labels = words.to(device), covarep.to(device), facet.to(
                 device), inputLen.to(device), labels.to(device)
             optimizer.zero_grad()
