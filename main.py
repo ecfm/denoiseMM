5efccca03c6c951d8016e00077c17f29a092ffef
mao <maocf1993@gmail.com>
2019-12-05 21:44:47 -0500

    rm mask train

--- main.py
+++ main.py
@@ -11,7 +11,6 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import torch.utils.data as Data
-from sklearn.metrics import accuracy_score, f1_score
 
 from consts import global_consts as gc
 from masked_dataset import MaskedDataset
@@ -150,10 +149,6 @@ def train_model(args, config_file_name, model_name):
         for i, data in enumerate(train_loader):
             optimizer.zero_grad()
             words, covarep, facet, inputLen, labels = data
-            mask_ratio = np.random.choice([0, 0.2, 0.4], p=[0.7, 0.2, 0.1])
-            if mask_ratio > 0:
-                m = np.random.choice([0, 1], p=[mask_ratio, 1 - mask_ratio], size=(words.shape[0], words.shape[1], 1))
-                words = words * torch.tensor(m.astype(np.float32))
             words, covarep, facet, inputLen, labels = words.to(device), covarep.to(device), facet.to(device), \
                                                       inputLen.to(device), labels.to(device)
             if covarep.size()[0] == 1:
