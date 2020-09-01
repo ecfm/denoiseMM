607148c5342bfd8c3596f2cde700b9d28e177227
Chengfeng Mao <maocf1993@gmail.com>
2020-09-01 12:33:31 -0400

    fix

--- src/models/my_model_av2l_loss/model.py
+++ src/models/my_model_av2l_loss/model.py
@@ -212,6 +212,7 @@ class Model(nn.Module):
                     _, outputs = self(x_l=words, x_a=covarep, x_v=facet)
                 else:
                     outputs = self(x_l=words, x_a=covarep, x_v=facet)
+                output_all.extend(labels.tolist())
                 label_all.extend(outputs.tolist())
             metrics = self.ds.eval(output_all, label_all)
             return metrics, output_all
