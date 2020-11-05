473e5b452389c6ab12a8ce2f7dfd012868df828b
Chengfeng Mao <maocf1993@gmail.com>
2020-11-05 16:45:26 -0500

    debug

--- src/models/ablation_my_model_no_av2l/model.py
+++ src/models/ablation_my_model_no_av2l/model.py
@@ -51,10 +51,12 @@ class Model(nn.Module):
         """
         text, audio, and vision should have dimension [batch_size, seq_len, n_features]
         """
+        # [batch_size,  dim_l, seq_len]
         words = F.dropout(x_l.transpose(1, 2), p=0.25, training=self.training)
+        # [seq_len, batch_size,  d_l]
         words = self.proj_l(words).permute(2, 0, 1)
         if self.mode == L_MODE:
-            return words
+            return self.dec_l(words[0, :, :])
         l_latent, outputs_l = self.dec_l(self.enc_l(words)[-1])
         if self.mode == L_MODE:
             return outputs_l
